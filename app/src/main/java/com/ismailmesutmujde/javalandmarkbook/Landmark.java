4c307bc67d3c0ec381429d3e46861198b9924f2c
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-26 14:02:34 +0300

    updated: modeling

---
+++ app/src/main/java/com/ismailmesutmujde/javalandmarkbook/Landmark.java
@@ -0,0 +1,17 @@
+package com.ismailmesutmujde.javalandmarkbook;
+
+public class Landmark {
+
+    String name;
+    String country;
+    int image;;
+
+    // Constructor
+
+
+    public Landmark(String name, String country, int image) {
+        this.name = name;
+        this.country = country;
+        this.image = image;
+    }
+}
--- app/src/main/java/com/ismailmesutmujde/javalandmarkbook/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javalandmarkbook/MainActivity.java
@@ -3,13 +3,30 @@ package com.ismailmesutmujde.javalandmarkbook;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        landmarkArrayList = new ArrayList<>();
+
+        // Data
+        Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel", "France", R.drawable.eiffel);
+        Landmark colosseum = new Landmark("Colosseum", "Italy", R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.londonbridge);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(colosseum);
+        landmarkArrayList.add(londonBridge);
 
     }
 }
